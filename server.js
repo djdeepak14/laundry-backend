37dd857c71f73ed4b0b0deccea1812987642d68f
Deepak Khanal <djdeepakdj14@gmail.com>
2025-09-17 20:28:23 +0300

    updated

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
 app.use(cors({
   origin: [
     FRONTEND_URL.replace(/\/$/, ''),
-    'https://laundry-frontend-nine.vercel.app',
+    'https://laundry-frontend-git-main-djdeepak14s-projects.vercel.app',
     'http://localhost:3000',
     'http://localhost:3001',
   ],
