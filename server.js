e14745695cfdd93bdbc40bc4967b9d82d176197a
Deepak Khanal <djdeepakdj14@gmail.com>
2025-09-17 21:50:54 +0300

    updated the login

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
 app.use(cors({
   origin: [
     FRONTEND_URL.replace(/\/$/, ''),
-    'https://laundry-frontend-git-main-djdeepak14s-projects.vercel.app',
+    'https://https://laundry-frontend-nine.vercel.app/login',  
     'http://localhost:3000',
     'http://localhost:3001',
   ],
