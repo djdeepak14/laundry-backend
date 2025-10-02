590da8a70041673ff1551813b33f1cbb83753170
Rishi Ram Rijal <ramrijalrishi@gmail.com>
2025-10-02 08:45:15 +0200

    added router for booking with routes

---
+++ src/routes/booking.router.js
@@ -0,0 +1,21 @@
+import { Router } from "express";
+import { createBooking, cancelBooking, PastBookings, UpcomingBookings } from "../controllers/booking.controller.js";
+const router = Router()
+
+router.route("/")
+    .post(createBooking);
+
+router.route("/")
+    .post(createBooking)
+
+router.route("/:id")
+    .delete(cancelBooking)
+
+router.route("/past")
+    .get(PastBookings)
+
+router.route("/upcoming")
+    .get(UpcomingBookings)
+
+
+export default router
