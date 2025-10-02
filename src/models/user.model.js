851eac25bfc26a57b465d7a71fe595053a21bb69
Rishi Ram Rijal <ramrijalrishi@gmail.com>
2025-10-02 08:28:24 +0200

    changed the user default role(temp)

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -23,7 +23,7 @@ const userSchema = new mongoose.Schema({
     role: {
         type: String,
         enum: ["user", "admin"],
-        default: "user"
+        default: "admin"
     },
     refreshToken: {
         type: String
