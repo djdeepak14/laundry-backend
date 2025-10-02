0f5ecafa49061cb39985d6e75a23c039ab6fb402
Rishi Ram Rijal <ramrijalrishi@gmail.com>
2025-10-02 07:58:46 +0200

    bug fix on import

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser, getCurrentUser } from "../controllers/user.controller";
+import { registerUser, loginUser, logoutUser, getCurrentUser } from "../controllers/user.controller.js";
 
 const router = Router()
 
